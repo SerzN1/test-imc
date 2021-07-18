ca3a40595ea25d15b6888f1dc9dbd6a04d3ec7f8
serzn1 <bob_s@list.ru>
2021-07-18 13:32:04 +0200

    chore: Add Filter component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/statistics/i);
+  expect(headerElement).toBeInTheDocument();
+  const footerElement = screen.getByText(/copyright/i);
+  expect(footerElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
+import {Filter} from './components/Filter/Filter'
 import './App.css';
 
+const PERIOD_FILTER = ['week', 'month']
+const VALUE_TYPE_FILTER = ['revenue', 'margin']
+
 function App() {
+  const [period, setPeriod] = useState(PERIOD_FILTER[0])
+  const [valueType, setValueType] = useState(VALUE_TYPE_FILTER[0])
+
   return (
     <>
       <header className="header">
@@ -11,7 +18,19 @@ function App() {
       </header>
       <main className="main">
         <div className="container">
+          <section className="row">
+            <div className="col">
+              <Filter filters={PERIOD_FILTER} selectedFilter={period} onFilterSelected={setPeriod} />
+            </div>
+            <div className="col">
+              <Filter filters={VALUE_TYPE_FILTER} selectedFilter={valueType} onFilterSelected={setValueType} />
+            </div>
+          </section>
 
+          <section>
+            {period}
+            {valueType}
+          </section>
         </div>
       </main>
       <footer className="footer">

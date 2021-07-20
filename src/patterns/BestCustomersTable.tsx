2f345350aa00a5b7ed6f9d0d7f207c58214c9554
serzn1 <bob_s@list.ru>
2021-07-20 21:21:10 +0200

    chore: Add pagination to Our best customers table

--- src/patterns/BestCustomersTable.tsx
+++ src/patterns/BestCustomersTable.tsx
@@ -1,6 +1,7 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { useBestCustomers } from '../api/useBestCustomers';
 import { Table, TableHeader, TableBody } from '../components/Table/Table';
+import { Pagination } from '../components/Pagination/Pagination';
 import { ValueTypeFilter } from '../constant';
 
 interface BestCustomersTableProps {
@@ -8,10 +9,19 @@ interface BestCustomersTableProps {
 }
 
 const nf = new Intl.NumberFormat();
+const PER_PAGE_LIMIT = 15;
 
 export const BestCustomersTable: React.FC<BestCustomersTableProps> = memo(
   ({ valueType }) => {
+    const [page, setPage] = useState(0);
+    const [tableData, setTableData] = useState([]);
     const { isLoading, data, error } = useBestCustomers();
+
+    useEffect(() => {
+      setTableData(
+        data.slice(PER_PAGE_LIMIT * page, PER_PAGE_LIMIT * (page + 1))
+      );
+    }, [data, page]);
 
     if (isLoading) {
       return <div>...is loading</div>;
@@ -29,33 +39,42 @@ export const BestCustomersTable: React.FC<BestCustomersTableProps> = memo(
     ];
 
     return (
-      <Table>
-        <TableHeader headerItems={bestCustomersTableHeader} />
-        <TableBody>
-          {data.map(
-            ({
-              customer_id,
-              customer_name,
-              customer_region,
-              invoices_count,
-              total_margin,
-              total_revenue,
-            }: ICustomerAPI) => (
-              <tr key={customer_id}>
-                <td align="left">{customer_id}</td>
-                <td>{customer_name}</td>
-                <td className="d-sm-hidden">{customer_region}</td>
-                <td align="right">{invoices_count}</td>
-                <td align="right">
-                  {valueType === ValueTypeFilter.margin
-                    ? nf.format(total_margin)
-                    : nf.format(total_revenue)}
-                </td>
-              </tr>
-            )
-          )}
-        </TableBody>
-      </Table>
+      <>
+        <Table>
+          <TableHeader headerItems={bestCustomersTableHeader} />
+          <TableBody>
+            {tableData.map(
+              ({
+                customer_id,
+                customer_name,
+                customer_region,
+                invoices_count,
+                total_margin,
+                total_revenue,
+              }: ICustomerAPI) => (
+                <tr key={customer_id}>
+                  <td align="left">{customer_id}</td>
+                  <td>{customer_name}</td>
+                  <td className="d-sm-hidden">{customer_region}</td>
+                  <td align="right">{invoices_count}</td>
+                  <td align="right">
+                    {valueType === ValueTypeFilter.margin
+                      ? nf.format(total_margin)
+                      : nf.format(total_revenue)}
+                  </td>
+                </tr>
+              )
+            )}
+          </TableBody>
+        </Table>
+        {data.length > PER_PAGE_LIMIT ? (
+          <Pagination
+            page={page}
+            pages={Math.round(data.length / PER_PAGE_LIMIT)}
+            onPageChange={setPage}
+          />
+        ) : null}
+      </>
     );
   }
 );
